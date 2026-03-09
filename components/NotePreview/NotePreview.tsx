cc70bba6d1e059421617ab0b386ad6d4f16f735f
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-03-10 00:40:14 +0100

    change NotePreview

--- components/NotePreview/NotePreview.tsx
+++ components/NotePreview/NotePreview.tsx
@@ -1,6 +1,7 @@
-// components/NotePreview/NotePreview.client.tsx
+
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import { Note } from "@/types/note";
@@ -10,20 +11,40 @@ interface NotePreviewProps {
 }
 
 export default function NotePreview({ noteId }: NotePreviewProps) {
+
+  const router = useRouter();
   const { data: note, isLoading, isError } = useQuery<Note>({
     queryKey: ["note", noteId],
     queryFn: () => fetchNoteById(noteId),
     refetchOnMount: false, 
   });
 
+  const handleClose = () => {
+    router.back();
+  };
+
   if (isLoading) return <p>Loading note...</p>;
   if (isError || !note) return <p>Error loading note.</p>;
 
   return (
-    <div>
+    <div style={{ position: "relative", padding: "24px" }}>
+      <button
+        onClick={handleClose}
+        style={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          cursor: "pointer",
+        }}
+      >
+        Close
+      </button>
+
       <h2>{note.title}</h2>
       <p>{note.content}</p>
-      <p>Tag: {note.tag}</p>
+      <p>
+        <strong>Tag:</strong> {note.tag}
+      </p>
     </div>
   );
 }
